ca2d5146f5cd6c16bc25d7c752202a8ddda9b958
Gustavo Kowalczuk Felicetti <428968@unoesc.lan>
2024-08-14 22:05:26 -0300

    Comandos, primeiros códigos Csharp

---
+++ Atividades/Aula 01/Program.cs
@@ -0,0 +1,18 @@
+﻿// Comentário de uma linha
+
+/*
+    Comentário de
+    Múltiplas linhas
+*/
+
+// Declarando variável
+int number;
+
+// Atribuindo valor
+number = 10;
+
+// Declarando contante
+const int DAYS_IN_WEEK = 7;
+
+// Imprimindo String concatenada
+Console.WriteLine($"A semana tem {DAYS_IN_WEEK} dias.");
