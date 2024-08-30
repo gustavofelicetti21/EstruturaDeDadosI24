88edd6a8d5000701cedd5af68963e9bd573b00fc
gustavofelicetti21 <gustavokfelicetti@gmail.com>
2024-08-30 09:38:06 -0300

    Resolução do exercício passado na aula

--- Atividades/Aula03-Exercicio/Program.cs
+++ Atividades/Aula03-Exercicio/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int j=0;
+int[] num = new int[50];
+
+for (int i = 1; i<=100; i++) {
+    if ((float)(i%2)==0)
+    {
+        num[j] = i;
+        j++;
+    }
+}
+
+foreach (int n in num)
+{
+    Console.WriteLine(n);
+}
