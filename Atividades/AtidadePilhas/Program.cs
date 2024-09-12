705a81ff70ee6fd6b98e8361ce1fad44f12586f8
gustavofelicetti21 <gustavokfelicetti@gmail.com>
2024-09-11 22:10:52 -0300

    Stacks, e Atividade sobre Pilhas

---
+++ Atividades/AtidadePilhas/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Digite uma palavra: ");
+string? palavra = Console.ReadLine();
+
+Stack<char> caracteres = new Stack<char>();
+foreach (char p in palavra)
+{
+    caracteres.Push(p);
+}
+
+string invertido = string.Empty;
+while (caracteres.Count > 0)
+{
+    invertido += caracteres.Pop();
+}
+
+if(invertido == palavra) {
+    Console.WriteLine("É palindromo");
+}
+else {
+    Console.WriteLine("Não é palindromo");
+}
