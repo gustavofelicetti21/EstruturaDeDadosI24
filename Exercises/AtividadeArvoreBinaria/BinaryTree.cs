639b3fae57f8ac631c16a7875fbae13403c3ca4a
gustavofelicetti21 <gustavokfelicetti@gmail.com>
2024-11-13 21:35:30 -0300

    Atividade Arvore Binaria

---
+++ Exercises/AtividadeArvoreBinaria/BinaryTree.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AtividadeArvoreBinaria
+{
+    public class BinaryTree<T>
+    {
+        public BinaryTreeNode<T>? Root { get; set; }
+    }
+}
